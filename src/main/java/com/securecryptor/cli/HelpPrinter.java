eab1fcb60a2ac016259e816f49d414340fb3eba5
goodwill-sakhile <116113833+goodwill-sakhile@users.noreply.github.com>
2025-06-25 00:37:00 +0200

    HelpPrinter Script

---
+++ src/main/java/com/securecryptor/cli/HelpPrinter.java
@@ -0,0 +1,15 @@
+package com.securecryptor.cli;
+
+/**
+ * Displays help and usage information to the user.
+ */
+public class HelpPrinter {
+
+    public static void printHelp() {
+        System.out.println("\n=== SecureCryptor Help ===");
+        System.out.println("1 - Encrypt file");
+        System.out.println("2 - Decrypt file");
+        System.out.println("3 - Exit");
+        System.out.println("Additional features coming soon!");
+    }
+}
